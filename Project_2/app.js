e06150c1e3842a775310737b6f2e9e2253995cc9
georgeacp <133732710+georgeacp@users.noreply.github.com>
2025-07-28 21:07:59 -0500

    feat: Set title and interactive paragraph

---
+++ Project_2/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Juego del número secreto";
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Indicar un número entre 1 y 10";
