bba4e3863a52c67536b6e4086b79bc494326a453
georgeacp <133732710+georgeacp@users.noreply.github.com>
2025-07-28 21:42:24 -0500

    feat: Add secret number generation

--- Project_2/app.js
+++ Project_2/app.js
@@ -1,9 +1,17 @@
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento, texto) {
   let elementoHTML = document.querySelector(elemento);
   elementoHTML.innerHTML = texto;
+  return;
 }
 
 function intentoDeUsuario() {}
 
+function generarNumeroSecreto() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
 asignarTextoElemento("h1", "Juego del número secreto");
-asignarTextoElemento("p", "Indicar un número entre 1 y 100");
+asignarTextoElemento("p", "Indicar un número entre 1 y 10");
